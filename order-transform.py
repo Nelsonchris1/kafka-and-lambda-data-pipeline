aa0222bdb8fa48a0c371e7c8b1b8014d36ba4b50
Nelsonchris1 <ogbeide331@gmail.com>
2022-04-25 10:02:17 +0100

    fixed file path for deployment

--- order-transform.py
+++ order-transform.py
@@ -5,12 +5,14 @@ import json
 import random 
 import os
 
+#Next add bucket path for s3_upload
 
 def s3_file_name(key):
-    file_name = key.replace('.','/').split('.')[-2]
+    #file_name = key.replace('.','/').split('.')[-2]
+    file_name = key.split('.')[0]
     return file_name
 
-def process_data(data):
+def clean_data(data):
     order_df = pd.json_normalize(data)
     columns = ['itemid', 'orderid', 'orderunits', 'ordertime', 'zipcode', 'city', 'state']
     order_df.columns = columns
@@ -26,16 +28,17 @@ def process_data(data):
 
 
 def process_orders(event, context):
-    # bucket = event['detail']['bucket']['name']
-    # key = event['detail']['object']['key']
-    # s3_buc = "s3://" + bucket + "/" + key
-    key = 'customer-orders+0+0000000000.json'
+    bucket = event['detail']['bucket']['name']
+    key = event['detail']['object']['key']
+    s3_buc = "s3://" + bucket + "/" + key
+    # key = 'customer-orders+0+0000000000.json'
     file_name = s3_file_name(key)
-    output_name = 'customer_file' + str(random.randint(1111,9999)) + '.csv'
-    data = [json.loads(line) for line in open(key, 'r')]
-    data_length, order_df = process_data(data)
+    output_name = 'orders_' + str(random.randint(1111,9999)) + '.csv'
+    data = [json.loads(line) for line in open(s3_buc, 'r')]
+    data_length, order_df = clean_data(data)
     records_per_run(file_name=file_name, data_length=data_length, output_name=output_name)
-    order_df.to_csv(output_name, index=False)
+    output_path = 's3://confluent-customer-orders/landing/' + output_name
+    order_df.to_csv(output_path, index=False)
     print("Done")
 
 process_orders(None, None)
