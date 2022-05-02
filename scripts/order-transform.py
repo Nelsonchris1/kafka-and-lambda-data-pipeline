d35cf4a70bbab4895449d8ced858fef37593c26b
Nelsonchris1 <ogbeide331@gmail.com>
2022-05-02 10:59:30 +0100

    updated awsdatawrangler for record_run.py

--- scripts/order-transform.py
+++ scripts/order-transform.py
@@ -4,18 +4,26 @@ import datetime
 import json
 import random 
 import os
-# import awswrangler as wr
+import awswrangler as wr
+import boto3
 
 #Next add bucket path for s3_upload
 
 def s3_file_name(key):
-    #file_name = key.replace('.','/').split('.')[-2]
-    file_name = key.split('.')[0]
+    # Exract file name from bucket key
+ 
+    file_name = key.replace('.','/').split('.')[-2]
     return file_name
 
 def clean_data(data):
+    """
+    Normalize table and extract datetime features,
+    drop unneccesary column and fix column names
+    """
+
     order_df = pd.json_normalize(data)
-    columns = ['itemid', 'orderid', 'orderunits', 'ordertime', 'zipcode', 'city', 'state']
+    columns = ['itemid', 'orderid', 'orderunits', 
+                'ordertime', 'zipcode', 'city', 'state']
     order_df.columns = columns
     order_df['ordertime'] = pd.to_datetime(order_df['ordertime'], unit="ms")
     order_df['hour'] = order_df['ordertime'].dt.hour
@@ -29,30 +37,30 @@ def clean_data(data):
 
 
 def process_orders(event, context):
+    #Extract bucket name and key from payload
+
     bucket = event['detail']['bucket']['name']
     key = event['detail']['object']['key']
-    s3_buc = "s3://" + bucket + "/" + key
-    # key = 'customer-orders+0+0000000000.json'
-    file_name = s3_file_name(key)
-    output_name = 'orders_' + str(random.randint(1111,9999)) + '.csv'
-    data = [json.loads(line) for line in open(s3_buc, 'r')]
+    s3_buc = "s3://" + bucket + "/" + key  
+    
+    # Call filenamefunction to extract file name  
+    filename = s3_file_name(key)
+    outputname = 'orders_' + str(random.randint(1111,9999)) + '.csv'
+
+    #Extract the json datausing json loads
+    s3_client = boto3.client('s3')
+    result = s3_client.get_object(Bucket=bucket, Key=key)
+    data = [json.loads(line) for line in result['Body'].read().splitlines()]
+
+    #Call clean data function to extract data lenght and 
     data_length, order_df = clean_data(data)
-    records_per_run(file_name=file_name, data_length=data_length, output_name=output_name)
-    output_path = 's3://confluent-customer-orders/landing/' + output_name + '.csv'
-    # order_df.to_csv(output_path, index=False)
-    wr.s3.to_csv(df=order_df, path=output_path)
+
+    #Log record runs
+    records_per_run(file_name=filename,
+                    data_length=data_length, 
+                    output_name=outputname)
+
+    # Save cleaned csv data
+    output_path = 's3://confluent-customer-orders/landing/' + outputname + '.csv'
+    wr.s3.to_csv(order_df, output_path)
     print("Done")
-
-process_orders(None, None)
-
-
-
-
-
-
-
-
-# To do list
-# 1.  Create virtualenv with isreal aminu medium article
-# 2. add awswrangler with it.
-# 3. add layer and import function to see it it
--- scripts/record_run.py
+++ scripts/record_run.py
@@ -1,12 +1,13 @@
 import datetime
 import pandas as pd
+import awswrangler as wr
 
 def records_per_run(file_name, data_length, output_name):
     path = 's3://confluent-customer-orders/run_logs/record_run.csv'
-    record_run = pd.read_csv(path)
+    recrd_data = wr.s3.read_csv('s3://confluent-customer-orders/run_logs/record_run.csv')
     data_run = {'input_file_name': file_name,
-                 'Data_length': data_length, 
-                 'timestamp': datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y"),
-                 'output_file_name': output_name}
-    new_record = record_run.append(data_run, ignore_index=True)
-    new_record.to_csv(path, index=False)
+                     'Data_length': data_length, 
+                     'timestamp': datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y"),
+                     'output_file_name': output_name}
+    df = recrd_data.append(data_run, ignore_index=True)
+    wr.s3.to_csv(df, 's3://confluent-customer-orders/run_logs/record_run.csv', index=False)
